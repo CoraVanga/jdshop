74209a5e3bc714853f59800d3e5111cc94664cce
DinhTien24 <15520880@gm.uit.edu.vn>
2018-04-16 23:52:09 +0700

    view image product on user

--- views/main/shopper.php
+++ views/main/shopper.php
@@ -24,48 +24,25 @@ use yii\widgets\Pjax;
 						<div class="active item">
 							<ul class="thumbnails">
 								<?php
+									$i=0;
 									foreach ($dataProvider->models as $model) {
-										echo '<li class="span3"><div class="product-box"><span class="sale_tag"></span>';
+									echo '<li class="span3"><div class="product-box"><span class="sale_tag"></span>';
+									if ($model->getImageProducts()->one()) {
+										$image = $model->getImageProducts()->asArray()->one();
+									 		echo '<p><a href="product_detail.html"><img src="../images/product-images'.'/'.$image['link'].'" alt="" /></a></p>';
+									 	}
 										echo '<a href="#" class="title">'.$model->name.'</a><br/>';
 										echo '<p class="price">'.$model->price.' VNĐ</p>';
 										echo '</div></li>';
+										$i++;
+										if($i==8) break;
+										if ($i==4) {
+											echo '</div><div class="item"><ul class="thumbnails">';
+											$i=0;
+										}
 									}
 								?>									
-								<li class="span3">
-									<div class="product-box">
-										<span class="sale_tag"></span>
-										<p><a href="product_detail.html"><img src="themes/images/ladies/1.jpg" alt="" /></a></p>
-										<a href="product_detail.html" class="title">Ut wisi enim ad</a><br/>
-										<a href="products.html" class="category">Commodo consequat</a>
-										<p class="price">$17.25</p>
-									</div>
-								</li>
-								<li class="span3">
-									<div class="product-box">
-										<span class="sale_tag"></span>
-										<p><a href="product_detail.html"><img src="themes/images/ladies/2.jpg" alt="" /></a></p>
-										<a href="product_detail.html" class="title">Quis nostrud exerci tation</a><br/>
-										<a href="products.html" class="category">Quis nostrud</a>
-										<p class="price">$32.50</p>
-									</div>
-								</li>
-								<li class="span3">
-									<div class="product-box">
-										<p><a href="product_detail.html"><img src="themes/images/ladies/3.jpg" alt="" /></a></p>
-										<a href="product_detail.html" class="title">Know exactly turned</a><br/>
-										<a href="products.html" class="category">Quis nostrud</a>
-										<p class="price">$14.20</p>
-									</div>
-								</li>
-								<li class="span3">
-									<div class="product-box">
-										<p><a href="product_detail.html"><img src="themes/images/ladies/4.jpg" alt="" /></a></p>
-										<a href="product_detail.html" class="title">You think fast</a><br/>
-										<a href="products.html" class="category">World once</a>
-										<p class="price">$31.45</p>
-									</div>
-								</li>
-							</ul>
+								
 						</div>
 						<div class="item">
 							<ul class="thumbnails">
