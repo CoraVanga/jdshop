18bae6b1e179bb05e3d1609c4bf71e929a044be8
DinhTien24 <15520880@gm.uit.edu.vn>
2018-04-18 10:05:01 +0700

    impove index and update view product

--- views/product/update.php
+++ views/product/update.php
@@ -5,18 +5,23 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Product */
 
-$this->title = 'Update Product: ' . $model->name;
+$this->title = 'Sửa đổi thông tin sản phẩm: ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Products', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="product-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-        'modelImage'=>$modelImage,
-    ]) ?>
-
-</div>
+	<div class="col-lg-10">
+		<div class="card card-outline-primary">
+			<div class="card-header">
+				<h4 class="m-b-0 text-white"><?= Html::encode($this->title) ?></h4>
+			</div>
+			<div class="card-body">
+				<?= $this->render('_form', [
+					'model' => $model,
+					'modelImage'=>$modelImage,
+					]) ?>
+				</div>
+			</div>
+		</div>
+	</div>
