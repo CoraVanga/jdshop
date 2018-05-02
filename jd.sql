0a7b0388ef5e519eba0d207f7982a3e28b32c24b
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-05-02 14:52:28 +0700

    sql, kịch bản 1 Mất dữ liệu cập nhật (Lost update)

--- jd.sql
+++ jd.sql
@@ -106,3 +106,22 @@ CREATE TABLE discount_detail
   FOREIGN KEY (id_product) REFERENCES product(id)
 );
 
+---- Demo -- hệ quản trị CSDL
+
+-- 1 .  Mất dữ liệu cập nhật (Lost update)
+-- Tình trạng này xảy ra khi có nhiều hơn một giao tác cùng thực hiện cập nhật
+-- trên 1 đơn vị dữ liệu. Khi đó, tác dụng của giao tác cập nhật thực hiện sau
+-- sẽ đè lên tác dụng của thao tác cập nhật trước.
+-- => Kịch bản : 2 admin cùng cập nhật lại số tiền của một sản phẩm, hoặc thông tin
+-- khuyến mãi cho nhiều sản phẩm
+-- Vd : nhận yêu cầu giảm giá tiền của sản phầm A bớt đi 100k.
+
+CREATE TABLE big_sale
+(
+	id int Identity(1,1),
+	id_product int,
+	sl_bandau int,
+	sl_hientai int,
+	info nvarchar(100),
+	FOREIGN KEY (id_product) REFERENCES product(id),
+)
