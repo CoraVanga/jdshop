d9344df8f7d37722102aa4e2e30453a47741d260
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-05-07 19:49:48 +0700

    AppAsset.php

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,8 +21,30 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'assets-admin/css/lib/calendar2/semantic.ui.min.css',
+        'assets-admin/css/lib/calendar2/pignose.calendar.min.css',
+        'assets-admin/css/lib/owl.carousel.min.css',
+        'assets-admin/css/lib/owl.theme.default.min.css',
+        'assets-admin/css/helper.css',
+        'assets-admin/css/style.css',
     ];
     public $js = [
+        'assets-admin/js/lib/bootstrap/js/popper.min.js',
+        'assets-admin/js/jquery.slimscroll.js',
+        'assets-admin/js/sidebarmenu.js',
+        'assets-admin/js/lib/sticky-kit-master/dist/sticky-kit.min.js',
+        'assets-admin/js/lib/morris-chart/raphael-min.js',
+        'assets-admin/js/lib/morris-chart/morris.js',
+        'assets-admin/js/lib/morris-chart/dashboard1-init.js',
+        'assets-admin/js/lib/calendar-2/moment.latest.min.js',
+        'assets-admin/js/lib/calendar-2/semantic.ui.min.js',
+        'assets-admin/js/lib/calendar-2/prism.min.js',
+        'assets-admin/js/lib/calendar-2/pignose.calendar.min.js',
+        'assets-admin/js/lib/calendar-2/pignose.init.js',
+        'assets-admin/js/lib/owl-carousel/owl.carousel.min.js',
+        'assets-admin/js/lib/owl-carousel/owl.carousel-init.js',
+        'assets-admin/js/scripts.js',
+        'assets-admin/js/custom.min.js',
     ];
     public $depends = [
          'yii\web\YiiAsset',
