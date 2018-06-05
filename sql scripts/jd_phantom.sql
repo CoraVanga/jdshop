5b6d6ab296ed99da7bd3f1da37c45f4b3ce00ce8
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-06-05 14:50:56 +0700

    phantom

--- sql scripts/jd_phantom.sql
+++ sql scripts/jd_phantom.sql
@@ -1,7 +1,13 @@
 ﻿use jd
+--Là tình trạng mà một giao tác đang thao tác trên một tập dữ liệu nhưng giao
+--tác khác lại chèn thêm các dòng dữ liệu vào tập dữ liệu mà giao tác kia quan
+--tâm
+
 
 -- kịch bảng cập nhật hết tất cả sản phẩm thay đổi giá không cho thêm mới 
 ---
+Drop procedure CAPNHATSANPHAMDONGLOAT
+go
 create PROCEDURE CAPNHATSANPHAMDONGLOAT
 @ID_PRODUCT INT, @CHAN INT
 AS
@@ -14,15 +20,37 @@ BEGIN
 	waitfor delay '00:00:07'
 	commit tran
 END
+
+-----
+Drop procedure CAPNHATSANPHAMDONGLOAT_ERROR
+go
+create PROCEDURE CAPNHATSANPHAMDONGLOAT_ERROR
+@ID_PRODUCT INT, @CHAN INT
+AS
+BEGIN 
+	--SET TRANSACTION ISOLATION LEVEL Serializable
+	begin tran
+	UPDATE product_detail
+	SET price = price + @CHAN
+	WHERE product_detail.id_product = @ID_PRODUCT
+	waitfor delay '00:00:07'
+	commit tran
+END
 ---- test --  bỏ qua file query khác
 	use jd
+
+Drop procedure chendulieuvao
+go
+create PROCEDURE chendulieuvao
+AS
+BEGIN
     SET IDENTITY_INSERT product_detail ON
 	begin tran
-	insert product_detail (id,size,price,amount,id_product) values (1000,10,500000,100,1)
+		insert product_detail (id,size,price,amount,id_product) values (1000,10,500000,100,1)
 	commit tran
+END
 --
 
-DROP PROC CAPNHATSANPHAMDONGLOAT
 EXEC CAPNHATSANPHAMDONGLOAT 1, 200
 ---
 
@@ -35,3 +63,11 @@ SELECT TOP 1000 [id]
       ,[amount]
       ,[id_product]
   FROM [JD].[dbo].[product_detail]
+
+-- Run correct 
+EXEC CAPNHATSANPHAMDONGLOAT 1, 200
+EXEC chendulieuvao
+-- Run Error
+EXEC CAPNHATSANPHAMDONGLOAT_ERROR 1, 200
+EXEC chendulieuvao
+
