acc955bdc9950b010f2e84492772c8ce4e113f76
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-06-05 12:22:32 +0700

    deadlock & dirty read

--- sql scripts/deadlock.sql
+++ sql scripts/deadlock.sql
@@ -23,11 +23,11 @@ create PROCEDURE XOACHITIETHOADON
 @ID_ORDER_LINE INT
 AS
 BEGIN
-	BEGIN TRY  
+	--BEGIN TRY  
 		DECLARE @ID_SALE_ORDER INT
 		DECLARE @PRICE INT
 		SET @PRICE = 0
-			SET TRANSACTION ISOLATION LEVEL Serializable
+			--SET TRANSACTION ISOLATION LEVEL Serializable ---- deadlock
 			BEGIN TRAN 
 				SELECT @ID_SALE_ORDER =  [order_line].[id_bill] FROM [order_line] WHERE [order_line].[id] = @ID_ORDER_LINE
 				DELETE FROM [order_line] WHERE  [order_line].[id] = @ID_ORDER_LINE   
@@ -38,7 +38,7 @@ BEGIN
 				IF @PRICE = 0
 					BEGIN
 						waitfor delay '00:00:15'
-						UPDATE [sale_order] SET [sale_order].[status] = 5 WHERE [sale_order].[id] = @ID_SALE_ORDER
+						UPDATE [sale_order] SET [sale_order].[status] = 0 WHERE [sale_order].[id] = @ID_SALE_ORDER
 						PRINT @PRICE
 					END
 				ELSE
@@ -49,20 +49,12 @@ BEGIN
 						WHERE [sale_order].[id] = @ID_SALE_ORDER
 						PRINT @PRICE
 					END
-	END TRY  
-	BEGIN CATCH  
-		 print 'ER'
-	END CATCH  
-			--IF @@ERROR= 0
-			--  BEGIN
-			--	commit Tran
-			--	select 0
-			--  END
-			--else
-			--  BEGIN
-			--	rollback Tran
-			--	select -1
-			--  END
+			commit Tran
+	--END TRY  
+	--BEGIN CATCH  
+		 --print 'ER'
+	--END CATCH  
+			
 END
 
 DROP PROC XOACHITIETHOADON
@@ -78,9 +70,9 @@ EXEC XOACHITIETHOADON 5
 ---0: Hủy
 -----------------------------------------------------------------------
 ---- Cập nhật trạng thái hoá đơn --
---TH1:  Nếu trạng thái cũ là 4 HOẶC 5 HOẶC Nếu trạng thái mới bằng trạng thái cũ thì roll back.
---TH2: Nếu trạng thái mới khác 5 và trạng thái cũ khác 4 thì chỉ cập nhật trạng thái.
---TH3: Nếu trạng thái mới là 5 và trạng thái cũ là  1,2,3 thì xoá hết chi tiết hoá đơn của hoá đơn đó.
+--TH1:  Nếu trạng thái cũ là 4 HOẶC 0 HOẶC Nếu trạng thái mới bằng trạng thái cũ thì roll back.
+--TH2: Nếu trạng thái mới khác 0 và trạng thái cũ khác 4 thì chỉ cập nhật trạng thái.(từ thanh toán qua huỷ)
+--TH3: Nếu trạng thái mới là 0 và trạng thái cũ là  1,2,3 thì xoá hết chi tiết hoá đơn của hoá đơn đó.
 -----------------------------------------------------------------------
 create PROCEDURE CAPNHATTRANGTHAIHOADON
 @ID_SALE_ORDER INT, @STATUS_NEW INT
@@ -91,17 +83,17 @@ BEGIN
 		--SET TRANSACTION ISOLATION LEVEL Serializable
 		BEGIN TRAN T2
 			SELECT @STATUS_OLD = [sale_order].[status] FROM [sale_order] WHERE  [sale_order].[id] = @ID_SALE_ORDER   
-			IF @STATUS_OLD = 4 OR @STATUS_OLD = 5 OR @STATUS_NEW = @STATUS_OLD
+			IF @STATUS_OLD = 4 OR @STATUS_OLD = 0 OR @STATUS_NEW = @STATUS_OLD
 			BEGIN 
 				ROLLBACK TRAN
 				PRINT 'TRƯỜNG HỢP 1' 
 			END
-			IF @STATUS_NEW != 5 AND ( @STATUS_OLD != 4 OR @STATUS_OLD != 5 ) -- chặn 5 nếu 5 thì phải xoá order_line
+			IF @STATUS_NEW != 0 AND ( @STATUS_OLD != 4 OR @STATUS_OLD != 0 ) -- chặn 0 nếu 0 thì phải xoá order_line
 			BEGIN 
 				UPDATE [sale_order] SET [sale_order].[status] = @STATUS_NEW WHERE [sale_order].[id] = @ID_SALE_ORDER  
 				PRINT 'TRƯỜNG HỢP 2' 
 			END
-			IF @STATUS_NEW =5  AND @STATUS_OLD != 4
+			IF @STATUS_NEW =0  AND @STATUS_OLD != 4
 			BEGIN
 				 UPDATE [sale_order] SET [sale_order].[status] = @STATUS_NEW WHERE [sale_order].[id] = @ID_SALE_ORDER
 			 
@@ -117,7 +109,7 @@ END
 
 DROP PROC CAPNHATTRANGTHAIHOADON
 
-EXEC CAPNHATTRANGTHAIHOADON 1,5
+EXEC CAPNHATTRANGTHAIHOADON 1,0
 --------------------------------------------------------------------
 SELECT TOP 1000 [total_price]
       ,[id]
@@ -142,84 +134,20 @@ SELECT TOP 1000 [id]
 
   ------------------------------------- Test deadlock -----------------------------------
 
-  -- test ko có SET TRANSACTION ISOLATION LEVEL Serializable
-CREATE PROCEDURE XOACHITIETHOADON 
-@ID_ORDER_LINE INT
-AS
-BEGIN
-	DECLARE @ID_SALE_ORDER INT
-	DECLARE @PRICE INT
-	SET @PRICE = 0
-		BEGIN TRAN t1
-			SELECT @ID_SALE_ORDER =  [order_line].[id_bill] FROM [order_line] WHERE [order_line].[id] = @ID_ORDER_LINE
-			DELETE FROM [order_line] WHERE  [order_line].[id] = @ID_ORDER_LINE   
-		
-			SELECT @PRICE = SUM([order_line].[sum_price]) FROM [order_line] WHERE [order_line].[id_bill] = @ID_SALE_ORDER 
-			PRINT @ID_SALE_ORDER
-			PRINT @PRICE
-			IF @PRICE = 0
-				BEGIN
-					UPDATE [sale_order] SET [sale_order].[status] = 5 WHERE [sale_order].[id] = @ID_SALE_ORDER
-					PRINT @PRICE
-				END
-			ELSE
-				BEGIN
-					UPDATE [sale_order]
-					SET [sale_order].[total_price] = @PRICE
-					WHERE [sale_order].[id] = @ID_SALE_ORDER
-					PRINT @PRICE
-				END
-		COMMIT TRAN t1 
-END
-
-DROP PROC XOACHITIETHOADON
-
-EXEC XOACHITIETHOADON 5
-----------------------------TEST--------------------------------
-create PROCEDURE CAPNHATTRANGTHAIHOADON
-@ID_SALE_ORDER INT, @STATUS_NEW INT
-AS
-BEGIN
-	DECLARE @STATUS_OLD INT 
-	BEGIN TRAN t2
-		SELECT @STATUS_OLD = [sale_order].[status] FROM [sale_order] WHERE  [sale_order].[id] = @ID_SALE_ORDER   
-		IF @STATUS_OLD = 4 OR @STATUS_OLD = 5 OR @STATUS_NEW = @STATUS_OLD
-		BEGIN 
-			ROLLBACK TRAN
-			PRINT 'TRƯỜNG HỢP 1' 
-		END
-		IF @STATUS_NEW != 5 AND ( @STATUS_OLD != 4 OR @STATUS_OLD != 5 ) -- chặn 5 nếu 5 thì phải xoá order_line
-		BEGIN 
-			UPDATE [sale_order] SET [sale_order].[status] = @STATUS_NEW WHERE [sale_order].[id] = @ID_SALE_ORDER  
-			PRINT 'TRƯỜNG HỢP 2' 
-		END
-		IF @STATUS_NEW =5  AND @STATUS_OLD != 4
-		BEGIN
-			 UPDATE [sale_order] SET [sale_order].[status] = @STATUS_NEW WHERE [sale_order].[id] = @ID_SALE_ORDER
-			 --waitfor delay '00:00:15'
-			 DELETE FROM [order_line] WHERE  [order_line].[id_bill] = @ID_SALE_ORDER  
-			 PRINT 'TRƯỜNG HỢP 3' 
-		END 
-	COMMIT TRAN t2
-END
---------------------------------
-DROP PROC CAPNHATTRANGTHAIHOADON
-
-EXEC CAPNHATTRANGTHAIHOADON 1,5
 
   -- Dữ liệu test --
   SET IDENTITY_INSERT [sale_order] off
-  INSERT INTO [sale_order] ([total_price],[id],[bill_code],[status],[created_date],[id_user])
-  VALUES ('40000',1000,'TESTBILL',1,NULL,10)
+  INSERT INTO [sale_order] ([total_price],[id],[status],[created_date],[id_user])
+  VALUES ('40000',1000,1,NULL,10)
   -----------------
   SET IDENTITY_INSERT [order_line] on
   -----------------------------------
-  INSERT INTO [order_line] ([id],[amount],[size_product],[sum_price],[code_color],[id_product],[id_bill])
-  VALUES (1000,100,9,20000,NULL,4,1000)
-  INSERT INTO [order_line] ([id],[amount],[size_product],[sum_price],[code_color],[id_product],[id_bill])
-  VALUES (1001,100,9,10000,NULL,3,1000)
-  INSERT INTO [order_line] ([id],[amount],[size_product],[sum_price],[code_color],[id_product],[id_bill])
-  VALUES (1002,100,9,15000,NULL,2,1000)
+  INSERT INTO [order_line] ([id],[amount],[size_product],[sum_price],[id_product],[id_bill])
+  VALUES (1000,100,9,20000,4,1000)
+  INSERT INTO [order_line] ([id],[amount],[size_product],[sum_price],[id_product],[id_bill])
+  VALUES (1001,100,9,10000,3,1000)
+  INSERT INTO [order_line] ([id],[amount],[size_product],[sum_price],[id_product],[id_bill])
+  VALUES (1002,100,9,15000,2,1000)
   -------------------------------------------------------------------------------
   UPDATE [sale_order] SET [sale_order].[status] = 1 WHERE [sale_order].[id] = 1000
   -------------------------------------------------------------------------------
@@ -228,13 +156,12 @@ EXEC CAPNHATTRANGTHAIHOADON 1,5
  
   EXEC XOACHITIETHOADON 1001
   -- chạy song song 
-   EXEC CAPNHATTRANGTHAIHOADON 1000,5
+   EXEC CAPNHATTRANGTHAIHOADON 1000,0
   --Msg 1205, Level 13, State 51, Procedure XOACHITIETHOADON, Line 22
   --Transaction (Process ID 51) was deadlocked on lock resources with another process and has been chosen as the deadlock victim. Rerun the transaction.
 --------------------------------------------------------------------
 SELECT TOP 1000 [total_price]
       ,[id]
-      ,[bill_code]
       ,[status]
       ,[created_date]
       ,[id_user]
@@ -245,9 +172,7 @@ SELECT TOP 1000 [id]
       ,[amount]
       ,[size_product]
       ,[sum_price]
-      ,[code_color]
       ,[id_product]
       ,[id_bill]
   FROM [JD].[dbo].[order_line]
 
-  
--- sql scripts/jd_dirtyread.sql
+++ sql scripts/jd_dirtyread.sql
@@ -1,5 +1,12 @@
+--Xảy ra khi một giao tác thực hiện đọc trên một đơn vị dữ liệu mà đơn vị dữ
+--liệu này đang bị cập nhật bởi một giao tác khác nhưng việc cập nhật chưa
+--được xác nhận
+--kịch bản: T1 cập nhật số lượng sản phẩm trong kho nhưng chưa commit thì T2 xem số lượng sản phẩm trong kho
 use jd
-alter procedure dirty_read
+
+drop procedure dirty_read_update
+go
+create procedure dirty_read_update
 @id int, @amount int
 as
 begin
@@ -9,19 +16,53 @@ begin
 		set amount = @amount
 		where id = @id
 		waitfor delay '00:00:10'
-	commit tran t1
+	rollback tran t1
 end
 
-ALTER procedure dirty_read_select
+---------------------------
+
+drop procedure dirty_read_select_error
+go
+create procedure dirty_read_select_error
 @id int
 as
 begin
-	set transaction isolation level repeatable read
-	begin tran t1
+	set transaction isolation level Read Uncommitted --error
+	begin tran t2
 	select * from product_detail where id = @id
-	commit tran t1
+	commit tran t2
 end
 
+drop procedure dirty_read_select
+go
+create procedure dirty_read_select
+@id int
+as
+begin
+	begin tran t2
+	select * from product_detail where id = @id
+	commit tran t2
+end
+-- Dữ liệu test --
+SET IDENTITY_INSERT product on
+--SET IDENTITY_INSERT product off
+INSERT INTO [product] ([id],[name],[status])
+VALUES (1000,N'Test product',1)
+-----------------
+SET IDENTITY_INSERT product_detail on
+-----------------------------------
+INSERT INTO product_detail([id],[id_product],[amount])
+VALUES (1000,1000,1000)
+-------------------------------------------------------------------------------
+update product_detail
+set amount = 1000
+where id=1000
 
-exec dirty_read 1, 1--TRAN 1
-exec dirty_read_select 1--TRAN 2
+--Xem dữ liệu
+select * from product_detail where id = 1000
+
+exec dirty_read_update 1000, 100--TRAN 1
+exec dirty_read_select 1000--TRAN 2
+----
+exec dirty_read_update 1000, 100--TRAN 1
+exec dirty_read_select_error 1000--TRAN 2
